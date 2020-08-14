bf6d74d0bcb93e1154f2868d8d4859458a75e68c
lonebots <jishnusurajila@gmail.com>
2020-08-15 02:51:10 +0530

    redux store completed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      {" "}
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
